4d0df7bf534b2f5337586fba938a961d7c57a52f
totallydillion <95979395+totallydillion@users.noreply.github.com>
2022-08-17 17:15:48 -0400

    fixed typings
    typings error bug fix

--- basics/GlobalCalculator.java
+++ basics/GlobalCalculator.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class GlobalCalculator {
     public static void main(String[] args) {
         
         //creating a listener to the number
