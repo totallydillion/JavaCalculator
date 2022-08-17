9c812ecc26ffed4a7ad1c37ab8100a0f3ee0238b
totallydillion <95979395+totallydillion@users.noreply.github.com>
2022-08-17 17:15:23 -0400

    With more symbols!
    Per Anubis Request, here is a calculator that contains more things, including but not limited to: addition, subtraction, multiplication.

---
+++ basics/GlobalCalculator.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //creating a listener to the number
+        Scanner userNumber = new Scanner(System.in);
+        
+        //asking the user for a number
+        System.out.println("Please enter a number");
+        
+        
+        //getting the number that the user has entered
+        int number1 = userNumber.nextInt();
+        
+        //getting another listener to the number
+        Scanner newNumber = new Scanner(System.in);
+        
+        //asking the user for another number
+        System.out.println("Please enter another number");
+        
+        //getting the number that the user entered
+        int number2 = newNumber.nextInt();
+        
+        System.out.println("Please enter a symbol");
+        
+        Scanner symbol = new Scanner(System.in);
+        
+        String expressionSymbol = symbol.next();
+        
+        switch(expressionSymbol){
+            case "+":
+            System.out.println(number1 + "+"+ number2 + "=" + number1 + number2);
+            break;
+            case "-":
+            System.out.println(number1 + "-"+ number2 + "=" + (number1 - number2));
+            break;
+            case "*":
+            System.out.println(number1 + "*"+ number2 + "=" + number1 * number2);
+            break;
+            case "/":
+            System.out.println(number1 + "/"+ number2 + "=" + number1 / number2);
+            break;
+            case "%":
+            System.out.println(number1 + "%"+ number2 + "=" + number1 %number2);
+            break;
+            case "^":
+            System.out.println(number1 + "^"+ number2 + "=" + (number1 ^ number2));
+            break;
+            
+            default:
+            System.out.println("That is not a valid symbol, Restarting.");
+            break;
+        }
+        return;
+    }
+}
